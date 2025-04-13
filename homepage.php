c2cd5dd0702e5e5f1e08740dd1dbeccdfdaf09a8
RIYA KUMARI <riyakumarisharma9@gmail.com>
2025-04-13 21:18:05 +0530

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+include("connect.php");
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>STUDY RESOURCES</title>
+    <link rel="stylesheet" href="homestyle.css">
+
+</head>
+<body>
+<h1>STUDY RESOURCES</h1>
+
+
+<a href="new_notes.html"><button>Notes</button></a> 
+<a href="new_pyqs.html"><button> PYQs</button></a> 
+<a href="new_mcqs.html"><button> MCQs</button></a> 
+
+
+<a href="logout.php">Logout</a>
+</div>
+</body>
+</html>
